0ccc92b79758054960d04cd9a6308e7eadf993fb
Shubham <shubhamwy@gmail.com>
2023-05-03 10:52:18 +0530

    first commit

---
+++ Static/_js.js
@@ -0,0 +1,829 @@
+//Toggle button function
+function togglemenu(){
+  document.getElementById('sidebar').classList.toggle('active');
+}
+
+
+// Slider range 
+var slider=document.getElementById("slider");
+var lab=document.getElementById("label-right");
+lab.innerHTML=slider.value/100;
+var value=(slider.value-slider.min)/(slider.max-slider.min)*100;
+slider.style.background='linear-gradient(90deg, rgb(78, 78, 192) '+value+'%, white 0%)';
+
+document.getElementById("slider").oninput= function(){
+  lab.innerHTML=slider.value/100;
+  value = (this.value-this.min)/(this.max-this.min)*100;
+  slider.style.background='linear-gradient(90deg, rgb(78, 78, 192) '+value+'%, white 0%)';
+}
+function updateTextInput(val) {
+  document.getElementById('x1').value=val/100; 
+}
+
+
+var slider2=document.getElementById("slider2");
+var lab2=document.getElementById("label-right2");
+lab2.innerHTML=slider2.value/100;
+var value2=(slider2.value-slider2.min)/(slider2.max-slider2.min)*100;
+slider2.style.background='linear-gradient(90deg, rgb(78, 78, 192) '+value2+'%, white 0%)';
+
+document.getElementById("slider2").oninput= function(){
+  lab2.innerHTML=slider2.value/100;
+  value2 = (this.value-this.min)/(this.max-this.min)*100;
+  slider2.style.background='linear-gradient(90deg, rgb(78, 78, 192) '+value2+'%, white 0%)';
+}
+function updateTextInput2(val) {
+  document.getElementById('y1').value=val/100; 
+}
+
+
+var slider3=document.getElementById("slider3");
+var lab3=document.getElementById("label-right3");
+lab3.innerHTML=slider3.value/100;
+var value3=(slider3.value-slider3.min)/(slider3.max-slider3.min)*100;
+slider3.style.background='linear-gradient(90deg, rgb(78, 78, 192) '+value3+'%, white 0%)';
+
+document.getElementById("slider3").oninput= function(){
+  lab3.innerHTML=slider3.value/100;
+  value3 = (this.value-this.min)/(this.max-this.min)*100;
+  slider3.style.background='linear-gradient(90deg, rgb(78, 78, 192) '+value3+'%, white 0%)';
+}
+function updateTextInput3(val) {
+  document.getElementById('z1').value=val/100; 
+}
+
+
+
+
+
+
+
+
+
+const DATA_COUNT = 101;
+const labels = [];
+for (let i = 0; i <DATA_COUNT; i++) {
+  var j=100*i;
+labels.push(j.toString());
+}
+var datapoints;
+const data = {
+labels: labels,
+datasets: [
+{
+  label: 'Cubic interpolation (monotone)',
+  data: datapoints,
+  //borderColor: Utils.CHART_COLORS.red,
+  fill: false,
+  cubicInterpolationMode: 'monotone',
+  tension: 0.4,
+  pointRadius:0
+}, {
+    label: 'Cubic interpolation',
+  data: datapoints,
+  //borderColor: Utils.CHART_COLORS.blue,
+  fill: false,
+  tension: 0.4,
+  pointRadius:0
+},
+]
+};
+
+const config = {
+type: 'line',
+data: data,
+options: {
+responsive: true,
+plugins: {
+  title: {
+    display: true,
+    text: 'Cubic interpolation',
+    color: 'white',
+  },
+},
+interaction: {
+  intersect: false,
+},
+scales: {
+
+  x: {
+    display: true,
+    title: {
+        display: true,
+        text: 'Time',
+        color:'white'
+    },
+    grid: {
+        display: true,
+        tickColor: 'white'
+      },
+        ticks: {
+          color: 'white'
+        },
+        border: {
+          color: 'white'
+        }
+    
+  },
+  y: {
+    display: true,
+    title: {
+      display: true,
+      text: 'Temperature',
+      color:'white'
+    },
+    grid: {
+   
+        display: true,
+        tickColor: 'white'
+      },
+      ticks: {
+          color: 'white',
+         
+        },
+        border: {
+          color: 'white'
+        },
+    
+  }
+}
+},
+};
+
+var mychart = new Chart(
+    document.getElementById('myChart'),
+    config
+    
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//time-temp graph
+function plot_graph(_arr){
+
+//arr_str = _arr;
+//arr_str = arr_str.slice(1,arr_str.length-2);
+//arr = arr_str.split(",");
+//arr = arr.map((item) => {return Number(item)});
+
+const DATA_COUNT = 101;
+const labels = [];
+for (let i = 0; i <DATA_COUNT; i++) {
+  var j=100*i;
+labels.push(j.toString());
+}
+const datapoints = _arr;
+const data = {
+labels: labels,
+datasets: [
+{
+  label: 'Cubic interpolation (monotone)',
+  data: datapoints,
+  //borderColor: Utils.CHART_COLORS.red,
+  fill: false,
+  cubicInterpolationMode: 'monotone',
+  tension: 0.4,
+  pointRadius:0
+}, {
+    label: 'Cubic interpolation',
+  data: datapoints,
+  //borderColor: Utils.CHART_COLORS.blue,
+  fill: false,
+  tension: 0.4,
+  pointRadius:0
+},
+]
+};
+
+const config = {
+type: 'line',
+data: data,
+options: {
+responsive: true,
+plugins: {
+  title: {
+    display: true,
+    text: 'Cubic interpolation',
+    color: 'white',
+  },
+},
+interaction: {
+  intersect: false,
+},
+scales: {
+
+  x: {
+    display: true,
+    title: {
+        display: true,
+        text: 'Time',
+        color:'white'
+    },
+    grid: {
+        display: true,
+        tickColor: 'white'
+      },
+        ticks: {
+          color: 'white'
+        },
+        border: {
+          color: 'white'
+        }
+    
+  },
+  y: {
+    display: true,
+    title: {
+      display: true,
+      text: 'Temperature',
+      color:'white'
+    },
+    grid: {
+   
+        display: true,
+        tickColor: 'white'
+      },
+      ticks: {
+          color: 'white',
+         
+        },
+        border: {
+          color: 'white'
+        },
+    
+  }
+}
+},
+};
+
+var mychart = new Chart(
+    document.getElementById('myChart'),
+    config
+    
+    );
+  return mychart;
+}
+
+
+
+
+
+
+
+
+    var temp="";
+    var t="";
+    var temp_val=Number(temp)
+    var t_val=Number(t);
+    var k = 2.61*(10**9)*Math.exp((-5819.7)/temp_val);
+    var q_val = 100 - k*(t_val/(40*24));
+    if(isNaN(q_val)){ var q=100; }
+    else var q=q_val.toFixed(2);
+      
+    const ctx=document.getElementById('myChart').getContext('2d');
+    const gradseg = ctx.createLinearGradient(0,0,450,0);
+    gradseg.addColorStop(0,'red');
+    gradseg.addColorStop(0.7,'yellow');
+    gradseg.addColorStop(1,'green');
+    
+    const data1 = {
+        labels: ['Mon', 'Tue'],
+        datasets: [{
+          label: 'Weekly Sales',
+          data: [q,100-q],
+          backgroundColor: [
+            gradseg,
+            'rgba(0,0,0,0.7)'
+          ],
+          borderColor: [
+            'rgba(255,255,255,1)',
+            'rgba(0,0,0,0.1)'
+          ],
+          borderWidth:5,
+          borderAlign:'inner',
+          cutout: '85%',
+          circumference:180,
+          rotation:270
+        }]
+        
+      };
+  
+      const gaugeChartText={
+        id: 'gaugeChartText',
+        afterDatasetsDraw(chart,args,pluginOptions){
+          const {ctx,data,chartArea:{top,bottom,left,right,width,height},scales:{r}} = chart;
+          ctx.save();
+          const xcor=chart.getDatasetMeta(0).data[0].x;
+          const ycor=chart.getDatasetMeta(0).data[0].y;
+  
+          //ctx.fillRect(xcor,ycor,200,1)
+          function textLabel(text,x,y,fontsize){
+            ctx.fillStyle='#FFFFFF';
+            ctx.font=`${fontsize}px sans-serif`;
+            ctx.fillText(text,x,y);
+          }
+          textLabel(q,xcor-60,ycor,50);
+          textLabel('0',left-20,ycor+7,20);
+          textLabel('100',right+4,ycor+7,20);
+          textLabel('50',xcor-10,top+45,20);
+          textLabel('25',left+35,top+115,20);
+          textLabel('75',right-60,top+115,20);
+        }
+      }
+    
+  
+      const config1 = {
+        type: 'doughnut',
+        data:data1,
+        options: {
+          layout:{
+            padding:{
+             left:20,
+             right:40,
+            }
+          },
+          aspectRatio:1.5,
+          plugins:{
+            legend:{
+              display:false
+            },
+            tooltip:{
+              enabled:false
+            }
+          }
+        },
+        plugins: [gaugeChartText]
+        
+       };
+  
+       var myChart = new Chart(
+        document.getElementById('Chart'),
+        config1
+      );
+  
+
+
+
+
+
+
+
+
+
+
+function gauge_chart(a,b){
+
+// quality
+
+  var temp=a;
+  var t=b;
+  console.log(t);
+  var temp_val=Number(temp)
+  var t_val=Number(t);
+  var k = 2.685*(10**9)*Math.exp((-48450.9567)/(temp_val*8.314));
+  var q_val = 100 - k*t_val;
+  if(isNaN(q_val)){ var q=100; }
+  else var q=q_val.toFixed(2);
+    
+  const ctx=document.getElementById('myChart').getContext('2d');
+  const gradseg = ctx.createLinearGradient(0,0,450,0);
+  gradseg.addColorStop(0,'red');
+  gradseg.addColorStop(0.7,'yellow');
+  gradseg.addColorStop(1,'green');
+  
+  const data1 = {
+      labels: ['Mon', 'Tue'],
+      datasets: [{
+        label: 'Weekly Sales',
+        data: [q,100-q],
+        backgroundColor: [
+          gradseg,
+          'rgba(0,0,0,0.7)'
+        ],
+        borderColor: [
+          'rgba(255,255,255,1)',
+          'rgba(0,0,0,0.1)'
+        ],
+        borderWidth:5,
+        borderAlign:'inner',
+        cutout: '85%',
+        circumference:180,
+        rotation:270
+      }]
+      
+    };
+
+    const gaugeChartText={
+      id: 'gaugeChartText',
+      afterDatasetsDraw(chart,args,pluginOptions){
+        const {ctx,data,chartArea:{top,bottom,left,right,width,height},scales:{r}} = chart;
+        ctx.save();
+        const xcor=chart.getDatasetMeta(0).data[0].x;
+        const ycor=chart.getDatasetMeta(0).data[0].y;
+
+        //ctx.fillRect(xcor,ycor,200,1)
+        function textLabel(text,x,y,fontsize){
+          ctx.fillStyle='#FFFFFF';
+          ctx.font=`${fontsize}px sans-serif`;
+          ctx.fillText(text,x,y);
+        }
+        textLabel(q,xcor-60,ycor,50);
+        textLabel('0',left-20,ycor+7,20);
+        textLabel('100',right+4,ycor+7,20);
+        textLabel('50',xcor-10,top+45,20);
+        textLabel('25',left+35,top+115,20);
+        textLabel('75',right-60,top+115,20);
+      }
+    }
+  
+
+    const config1 = {
+      type: 'doughnut',
+      data:data1,
+      options: {
+        layout:{
+          padding:{
+           left:20,
+           right:40,
+          }
+        },
+        aspectRatio:1.5,
+        plugins:{
+          legend:{
+            display:false
+          },
+          tooltip:{
+            enabled:false
+          }
+        }
+      },
+      plugins: [gaugeChartText]
+      
+     };
+
+     var myChart = new Chart(
+      document.getElementById('Chart'),
+      config1
+    );
+
+   return myChart;
+}
+    
+  
+var intervalid=setInterval(update_values,30000);
+    function update_values(){
+      $.getJSON($SCRIPT_ROOT+'/stuff',
+      function(data){
+       document.getElementById('_s1').value=(data.s1).toFixed(3);
+       document.getElementById('_s2').value=(data.s2).toFixed(3);
+      
+      });
+    };
+
+    const myForm = document.getElementById("myForm");
+  
+$('#abcd').on('click',function(e){
+   e.preventDefault();
+   $.ajax({
+    type:"POST",
+    url:"/predict",
+    data:{
+      sensor1:$("#_s1").val(),
+      sensor2:$("#_s2").val(),
+      x_val:$("#x1").val(),
+      y_val:$("#y1").val(),
+      z_val:$("#z1").val(),
+    },
+    success:function(data)
+      {
+        $("#pval").text(data.predicted_value);
+      }
+   })
+});
+
+
+var interval=setInterval(update_page,5000);
+ function update_page(){
+ // document.getElementById('abcd').click();
+ };  
+
+
+
+ const _qForm = document.getElementById("_qForm");
+  
+ $('#ab').on('click',function(e){
+    e.preventDefault();
+    $.ajax({
+     type:"POST",
+     url:"/_quality",
+     data:{
+       sensor1:$("#_s1").val(),
+       sensor2:$("#_s2").val(),
+       x_val:$("#x1").val(),
+       y_val:$("#y1").val(),
+       z_val:$("#z1").val(),
+       day:$("#_day").val(),
+       hour:$("#_hour").val(),
+     },
+     success:function(data)
+       {
+         myChart.destroy(); 
+         myChart =gauge_chart(data.predicted_value,data.t);
+       }
+    })
+ });
+ 
+ 
+ var interval=setInterval(update_page,5000);
+  function update_page(){
+  // document.getElementById('ab').click();
+  };  
+ 
+
+
+
+ $('#abc').on('click',function(e){
+  e.preventDefault();
+  $.ajax({
+   type:"POST",
+   url:"/plot",
+   data:{
+     sensor1:$("#_s1").val(),
+     sensor2:$("#_s2").val(),
+     x_val:$("#x1").val(),
+     y_val:$("#y1").val(),
+     z_val:$("#z1").val(),
+     day:$("#_day").val(),
+     hour:$("#_hour").val(),
+     second:$("#_second").val(),
+   },
+   
+   success:function(data)
+     {
+       mychart.destroy();
+       mychart=plot_graph(data._graph);
+     }
+    
+  })
+});
+
+var interval=setInterval(update_graph,10000);
+ function update_graph(){
+  //document.getElementById('abc').click();
+ // document.getElementById('abcde').click();
+  //document.getElementById('abcdef').click();
+ };
+
+ const cForm = document.getElementById("cForm");
+ $('#abcde').on('click',function(e){
+  e.preventDefault();
+  $.ajax({
+   type:"POST",
+   url:"/map",
+   data:{
+     sensor1:$("#_s1").val(),
+     sensor2:$("#_s2").val(),
+     z_value:$("#z2").val(),
+   },
+   
+   success:function(data)
+     {
+      $('#myImage').attr('src', "http://localhost:5000/"+data.image_url);
+     }
+    
+  })
+});
+
+
+
+//quality map:
+
+
+
+
+const data2 = {
+labels: [-2.25,-2.0, -1.75, -1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0.0, 0.25],
+datasets: []
+};
+
+const config2 = {
+type: 'line',
+data: data2,
+options: {
+responsive: true,
+
+interaction: {
+  intersect: false,
+},
+
+scales: {
+  x: {
+    display: true,
+    title: {
+        display: true,
+        text: 'X',
+        color:'white'
+    },
+    grid: {
+        display: true,
+        tickColor: 'white'
+      },
+        ticks: {
+          color: 'white'
+        },
+        border: {
+          color: 'white'
+        }
+    
+  },
+  y: {
+    display: true,
+    title: {
+      display: true,
+      text: 'Y',
+      color:'white'
+    },
+    grid: {
+   
+        display: true,
+        tickColor: 'white'
+      },
+      ticks: {
+          color: 'white',
+        },
+        border: {
+          color: 'white'
+        },
+    
+  }
+},
+plugins:{
+  legend:{
+    display:false
+  },
+title: {
+  display: true,
+  text: 'Quality (%)',
+  color: 'white',
+},
+},
+},
+};
+
+
+var qchart = new Chart(
+    document.getElementById('qChart'),
+    config2
+    );
+
+
+
+
+
+
+function quality(arr){
+
+y=[-4.65, -4.02, -3.45, -2.85, -2.25, -1.65, -1.05, -0.45, 0.15, 0.75];
+const data2 = {
+labels: [-2.25,-2.0, -1.75, -1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0.0, 0.25],
+datasets: [
+  { data: [NaN, -4.65, -4.65, -4.65, -4.65, -4.65, -4.65, -4.65, -4.65, -4.65,-4.65] ,showLine: false},
+  { data: [NaN,-4.02, -4.02, -4.02, -4.02, -4.02, -4.02, -4.02, -4.02, -4.02, -4.02] ,showLine: false},
+  { data: [NaN,-3.45, -3.45, -3.45, -3.45, -3.45, -3.45, -3.45, -3.45, -3.45, -3.45] ,showLine: false},
+  { data: [NaN,-2.85, -2.85, -2.85, -2.85, -2.85, -2.85, -2.85, -2.85, -2.85, -2.85] ,showLine: false},
+  { data: [NaN,-2.25, -2.25, -2.25, -2.25, -2.25, -2.25, -2.25, -2.25, -2.25, -2.25] ,showLine: false},
+  { data: [NaN,-1.65, -1.65, -1.65, -1.65, -1.65, -1.65, -1.65, -1.65, -1.65, -1.65] ,showLine: false},
+  { data: [NaN,-1.05, -1.05, -1.05, -1.05, -1.05, -1.05, -1.05, -1.05, -1.05, -1.05] ,showLine: false},
+  { data: [NaN,-0.45, -0.45, -0.45, -0.45, -0.45, -0.45, -0.45, -0.45, -0.45, -0.45] ,showLine: false},
+  { data: [NaN,0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15, 0.15] ,showLine: false},
+  { data: [NaN,0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75, 0.75] ,showLine: false},
+]
+};
+
+
+
+const config2 = {
+type: 'line',
+data: data2,
+options: {
+responsive: true,
+
+interaction: {
+  intersect: false,
+},
+
+scales: {
+  x: {
+    display: true,
+    title: {
+        display: true,
+        text: 'X',
+        color:'white'
+    },
+    grid: {
+        display: true,
+        tickColor: 'white'
+      },
+        ticks: {
+          color: 'white'
+        },
+        border: {
+          color: 'white'
+        }
+    
+  },
+  y: {
+    display: true,
+    title: {
+      display: true,
+      text: 'Y',
+      color:'white'
+    },
+    grid: {
+   
+        display: true,
+        tickColor: 'white'
+      },
+      ticks: {
+          color: 'white',
+        },
+        border: {
+          color: 'white'
+        },
+    
+  }
+},
+plugins:{
+  legend:{
+    display:false
+  },
+title: {
+  display: true,
+  text: 'Quality (%)',
+  color: 'white',
+},
+
+
+  tooltip:{
+
+    displayColors: false,
+    callbacks:{
+      title:function(){
+        return ``;
+      },
+      
+     label:((tooltipItem)=>{
+      // console.log(tooltipItem);
+       
+       for(let i=0;i<10;i++){
+        for(let j=0;j<10;j++){
+          if(tooltipItem.parsed.x==i+1 && tooltipItem.parsed.y==y[j]){ return `${arr[i][j]}%`; }
+         } }
+     })
+    }
+  },
+},
+},
+
+};
+
+var qchart = new Chart(
+    document.getElementById('qChart'),
+    config2
+    );
+
+    return qchart
+}
+  const qForm = document.getElementById("qForm");
+    $('#abcdef').on('click',function(e){
+     e.preventDefault();
+     $.ajax({
+      type:"POST",
+      url:"/quality",
+      data:{
+        sensor1:$("#_s1").val(),
+        sensor2:$("#_s2").val(),
+        z_value:$("#z2").val(),
+      },
+      
+      success:function(data)
+        {
+          qchart.destroy();
+         qchart=quality(data.arr);
+         console.log(data.arr)
+         console.log(typeof(data.arr[0]))
+        }
+       
+     })
+   });
---
+++ _app.py
@@ -0,0 +1,147 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from flask import Flask, request, render_template,jsonify
+import pickle
+from tensorflow import keras
+import random
+import cv2
+import math
+
+app=Flask(__name__)
+
+rel_model = keras.models.load_model('Model')
+scalerfile = 'scaler.sav'
+scaler = pickle.load(open(scalerfile, 'rb'))
+
+New_rel_model = keras.models.load_model('New_Model')
+New_scalerfile = 'New_scaler.sav'
+New_scaler = pickle.load(open(New_scalerfile, 'rb'))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/stuff",methods=["GET"])
+def stuff():
+    return jsonify(s1=np.random.uniform(285.617,308.003),s2=np.random.uniform(289.185,310.623))
+
+@app.route("/map",methods=["POST"])
+def map():
+    s1=request.form.get("sensor1")
+    s2=request.form.get("sensor2")
+    z=request.form.get("z_value")
+    _x=[-2.0, -1.75, -1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0.0, 0.25]
+    _y=[-4.65, -4.02, -3.45, -2.85, -2.25, -1.65, -1.05, -0.45, 0.15, 0.75]
+    temp=[]
+    for i in _x:
+        for j in _y:
+            brr=scaler.transform(np.array([[s1,s2,0,i,j,z]]))
+            temp.append([i,j,round(float(rel_model.predict(brr)),3)])
+
+    X, Y = np.meshgrid(np.linspace(-2.0, 0.25, 800), np.linspace(-4.65, 0.75, 500))
+
+    kernel_size = 1
+    temp_interp = np.zeros_like(X)
+    for i in range(len(temp)):
+        kernel = np.exp(-((X - temp[i][0])**2 + (Y - temp[i][1])**2) / (2 * kernel_size**2))
+        temp_interp += kernel * temp[i][2]
+    temp_interp = (temp_interp / temp_interp.max() * 255).astype(np.uint8)
+
+    color_map = cv2.applyColorMap(temp_interp, cv2.COLORMAP_JET)
+    cv2.imwrite("static/Temp.jpg", color_map)
+
+    image_url = 'static/Temp.jpg';
+    return jsonify({'image_url': image_url})
+
+
+@app.route("/quality",methods=["POST"])
+def quality():
+    s1=request.form.get("sensor1")
+    s2=request.form.get("sensor2")
+    z=request.form.get("z_value")
+    _x=[-2.0, -1.75, -1.5, -1.25, -1.0, -0.75, -0.5, -0.25, 0.0, 0.25]
+    _y=[-4.65, -4.02, -3.45, -2.85, -2.25, -1.65, -1.05, -0.45, 0.15, 0.75]
+    temp=[]
+    for i in _x:
+        ar=[]
+        for j in _y:
+            brr=New_scaler.transform(np.array([[s1,s2,i,j,z]]))
+            temp_val=round(float(New_rel_model.predict(brr)),3)
+            k = 2.685*(10**9)*math.exp((-48450.9567)/(temp_val*8.314))
+            q_val = 100 - k*(1)
+            ar.append(round(q_val,2))
+        temp.append(ar)
+
+    '''arr=[]
+    for i in _x:
+           ar=[]
+           for j in _y:
+                  ar.append(round(np.random.uniform(0,100),2))
+           arr.append(ar)'''
+  
+    #return jsonify({'arr':arr})
+    return jsonify({'arr':temp})
+
+#Temperature Prediction at a particular point
+@app.route("/predict",methods=["POST"])
+def predict():
+        
+       s1=float(request.form.get("sensor1"))
+       s2=float(request.form.get("sensor2"))
+       x=float(request.form.get("x_val"))
+       y=float(request.form.get("y_val"))
+       z=float(request.form.get("z_val"))
+ #      day=int(request.form.get("day"))
+ #      hour=int(request.form.get("hour"))
+ #      second=int(request.form.get("second"))
+  #     t=(24*3600*day+3600*hour+second)/90
+
+       arr=New_scaler.transform(np.array([[s1,s2,x,y,z]]))
+       prediction=round(float(New_rel_model.predict(arr)),5)
+
+ #      return jsonify({'predicted_value':prediction,'t':t})
+       return jsonify({'predicted_value':prediction})
+
+
+
+
+@app.route("/_quality",methods=["POST"])
+def _quality():
+        
+       s1=float(request.form.get("sensor1"))
+       s2=float(request.form.get("sensor2"))
+       x=float(request.form.get("x_val"))
+       y=float(request.form.get("y_val"))
+       z=float(request.form.get("z_val"))
+       day=int(request.form.get("day"))
+       hour=int(request.form.get("hour"))
+       t=day+hour/24
+
+       arr=New_scaler.transform(np.array([[s1,s2,x,y,z]]))
+       prediction=round(float(New_rel_model.predict(arr)),5)
+
+       return jsonify({'predicted_value':prediction,'t':t})
+
+
+
+
+@app.route("/plot",methods=["POST"])
+def plot():
+    
+       s1=float(request.form.get("sensor1"))
+       s2=float(request.form.get("sensor2"))
+       x=float(request.form.get("x_val"))
+       y=float(request.form.get("y_val"))
+       z=float(request.form.get("z_val"))
+
+       graph=[]
+       for i in range(101):
+            brr=scaler.transform(np.array([[s1,s2,100*i,x,y,z]]))
+            graph.append(round(float(rel_model.predict(brr)),5))
+
+       return jsonify({'_graph':graph})
+
+if __name__=="__main__":
+    app.run(debug=True)
+    
+    
